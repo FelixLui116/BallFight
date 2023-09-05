a156c48314a78893cee8b16ddc37928d2bda96db
FelixLui116 <37116340+FelixLui116@users.noreply.github.com>
2023-09-05 14:10:36 +0100

    update GameAPI

--- Assets/Script/OpenProjectAPI.cs
+++ Assets/Script/OpenProjectAPI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System;
 
 public class OpenProjectAPI : MonoBehaviour
 {
@@ -21,66 +22,73 @@ public class OpenProjectAPI : MonoBehaviour
     {
         UnityWebRequest www = UnityWebRequest.Get(gitHubJsonURL);
         yield return www.SendWebRequest();
-
-        if (www.result != UnityWebRequest.Result.Success)
+        try
         {
-            Debug.LogError("Failed to retrieve GitHub JSON: " + www.error);
-        }
-        else
-        {
-            // Successfully retrieved JSON data
-            string jsonText = www.downloadHandler.text;
-            Debug.Log("GitHub JSON Data: " + jsonText);
-
-            // Parse the JSON data
-            JSONData jsonData = JsonUtility.FromJson<JSONData>(jsonText);
-
-            // // Extract "BallFight" value
-            // string ballFightLine = jsonData.payload.blob.rawLines[1].Trim();
-            // string ballFightValue = ballFightLine.Split(':')[1].Trim(new char[] { ' ', '"', '\t' });
-            // Debug.Log("GitHub BallFight Value: " + ballFightValue);
-
-
-
-            string[] gameList = jsonData.payload.blob.rawLines;
-            
-            // string ballFightValue = jsonData.payload.blob.rawLines[1].Trim(); // This gets the third line of rawLines array
-            for (int i = 0; i < gameList.Length; i++)
+            if (www.result != UnityWebRequest.Result.Success)
             {
+                Debug.LogError("Failed to retrieve GitHub JSON: " + www.error);
+            }
+            else
+            {
+                // Successfully retrieved JSON data
+                string jsonText = www.downloadHandler.text;
+                Debug.Log("GitHub JSON Data: " + jsonText);
+
+                // Parse the JSON data
+                JSONData jsonData = JsonUtility.FromJson<JSONData>(jsonText);
+
+                // // Extract "BallFight" value
+                // string ballFightLine = jsonData.payload.blob.rawLines[1].Trim();
+                // string ballFightValue = ballFightLine.Split(':')[1].Trim(new char[] { ' ', '"', '\t' });
+                // Debug.Log("GitHub BallFight Value: " + ballFightValue);
+
+
+
+                string[] gameList = jsonData.payload.blob.rawLines;
                 
-                Debug.Log("GitHub BallFight Value: " + gameList[i]);
-                if (gameList[i].Contains(gameName)) // find game name and extract value
+                // string ballFightValue = jsonData.payload.blob.rawLines[1].Trim(); // This gets the third line of rawLines array
+                for (int i = 0; i < gameList.Length; i++)
                 {
-                    // // further parse the line to get the value
-                    // int startIndex = gameList[i].IndexOf('"') + 1;
-                    // int endIndex = gameList[i].LastIndexOf('"');
-                    // string value = gameList[i].Substring(startIndex, endIndex - startIndex);
                     
-                    // Debug.Log($"gameList: {gameList}  value: {value}"); // log gameList: System.String[]  value: BallFight": "200
-                    // // gameAPI_value = value;
-                    // break; // exit for loop
+                    Debug.Log("GitHub BallFight Value: " + gameList[i]);
+                    if (gameList[i].Contains(gameName)) // find game name and extract value
+                    {
+                        // // further parse the line to get the value
+                        // int startIndex = gameList[i].IndexOf('"') + 1;
+                        // int endIndex = gameList[i].LastIndexOf('"');
+                        // string value = gameList[i].Substring(startIndex, endIndex - startIndex);
+                        
+                        // Debug.Log($"gameList: {gameList}  value: {value}"); // log gameList: System.String[]  value: BallFight": "200
+                        // // gameAPI_value = value;
+                        // break; // exit for loop
 
-                    string[] parts = gameList[i].Split(':');
-                    // Extract the name and remove any extra whitespace and quotes
-                    string extractedName = parts[0].Trim('"', ' ', '\t', '\n', '\r');
+                        string[] parts = gameList[i].Split(':');
+                        // Extract the name and remove any extra whitespace and quotes
+                        string extractedName = parts[0].Trim('"', ' ', '\t', '\n', '\r');
 
-                    // Extract the value and remove any extra whitespace and quotes
-                    string extractedValue = parts[1].Trim('"', ' ', '\t', '\n', '\r');
+                        // Extract the value and remove any extra whitespace and quotes
+                        string extractedValue = parts[1].Trim('"', ' ', '\t', '\n', '\r');
 
-                    Debug.Log($"Extracted Game Name: {extractedName}  Value: {extractedValue}");
-                    // You can assign the extracted values to variables here if needed
-                    gameAPI_value = extractedValue;
-                    break; // Exit for loop
+                        Debug.Log($"Extracted Game Name: {extractedName}  Value: {extractedValue}");
+                        // You can assign the extracted values to variables here if needed
+                        gameAPI_value = extractedValue;
+                        break; // Exit for loop
+                    }
+                }
+
+                if(gameAPI_value == "200"){
+                    Debug.Log("Game API is 200");
+                }
+                else{
+                    Debug.Log("Game API is not 200");
+                    PauseGame();
                 }
             }
-
-            if(gameAPI_value == "200"){
-                Debug.Log("Game API is 200");
-            }
-            else{
-                Debug.Log("Game API is not 200");
-            }
-            
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error: " + e.Message);
+            PauseGame();
         }
     }
 
@@ -106,4 +114,10 @@ public class OpenProjectAPI : MonoBehaviour
     {
         
     }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0.0f; // pause game
+    }
+
 }
